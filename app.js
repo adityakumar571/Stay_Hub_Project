814281a2fc8621591f37635b00c818fa3534a29c
Aditya kumar <adityakumar.email@gmail.com>
2025-01-10 15:59:13 +0530

    listings rumove and set it to / home route

--- app.js
+++ app.js
@@ -77,10 +77,6 @@ const sessionOptions={
     httpOnly:true,
   },
 };
-app.get("/", (req, res) => {
-  res.send("i am root"); // Corrected response message
-});
-
 app.use(session(sessionOptions));
 app.use(flash());
 app.use(passport.initialize());
@@ -98,8 +94,8 @@ app.use((req,res,next)=>{
 })
 
 
-app.use("/listings",listings);
-app.use("/listings/:id/reviews",reviews);
+app.use("/",listings);
+app.use("/:id/reviews",reviews);
 app.use("/",userRouter);
 
  
